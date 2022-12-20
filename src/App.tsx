9581d5ec14a1d77b06922b92ffd49cb58f6a1bc6
Giclayne <giclayne@gmail.com>
2022-12-20 16:54:59 -0300

    importacao do props do botao submit

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <main className={styles.main}>
         <div>
           <h2>O que você vai fazer</h2>
-          <TaskForm />
+          <TaskForm btnText='Criar Tarefa'/>
         </div>
         <div>
           <h2>Suas tarefas</h2>
