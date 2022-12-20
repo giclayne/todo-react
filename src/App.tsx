3148980c2addbdd13893dd8932c087c9d72e7758
Giclayne <giclayne@gmail.com>
2022-12-20 17:44:46 -0300

    chamada dos states

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 //components
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -12,6 +12,7 @@ import styles from "./App.module.css"
 import { ITask } from './interfaces/Task';
 
 function App() {
+  const [taskList,setTaskList] = useState<ITask[]>([])
   return (
     <div className="App">
       <Header />
@@ -19,7 +20,7 @@ function App() {
       <main className={styles.main}>
         <div>
           <h2>O que você vai fazer</h2>
-          <TaskForm btnText='Criar Tarefa'/>
+          <TaskForm btnText='Criar Tarefa' taskList={taskList} setTaskList={setTaskList}/>
         </div>
         <div>
           <h2>Suas tarefas</h2>
