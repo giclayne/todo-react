f748066bd49388ce045a9c22176fb83c6d73923f
Giclayne <giclayne@gmail.com>
2022-12-20 16:47:29 -0300

    importacao dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import styles from "./App.module.css"
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 function App() {
   return (
     <div className="App">
       <Header />
 
       <main className={styles.main}>
-      <h1>Conteudo...</h1>
+        <div>
+          <h2>O que você vai fazer</h2>
+          <TaskForm />
+        </div>
+        <div>
+          <h2>Suas tarefas</h2>
+          <TaskList />
+        </div>
       </main>
       <Footer />
     </div>
