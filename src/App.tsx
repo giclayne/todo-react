284376c148a0133beaa111d7fc7d5128ecc3b3e6
Giclayne <giclayne@gmail.com>
2022-12-20 16:40:41 -0300

    importando css principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
-
+import styles from "./App.module.css"
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <main className={styles.main}>
       <h1>Conteudo...</h1>
+      </main>
       <Footer />
     </div>
   );
