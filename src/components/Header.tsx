113c492b0bde0490a97bcc4bbb8d1ba853b92cc1
Giclayne <giclayne@gmail.com>
2022-12-20 16:39:40 -0300

    importando css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import styles from "./Header.module.css"
 const Header = () => {
     return (
-        <header>
+        <header className={styles.header}>
             <h1>React + TS todo</h1>
         </header>
     )
